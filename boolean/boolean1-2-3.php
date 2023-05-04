3715d260c9c26ffce90e02af31de34daafa13020
Matkarim Matkarimov <matkarim1099@gmail.com>
2023-05-04 23:26:38 +0500

    04.05.23

---
+++ boolean/boolean1-2-3.php
@@ -0,0 +1,18 @@
+<?php
+//A butun soni berilgan. Jumlani rostlikka tekshiring: “A soni musbat".
+/*$a = -10;
+if ($a>0){
+    echo "a soni musbat";
+}
+else{
+    echo "a soni manfiy";
+}
+*/
+//A butun soni berilgan. Jumlani rostlikka tekshiring: “A soni toq son".
+$a = 3;
+if ($a%2!=0){
+    echo "a soni toq";
+}
+else{
+    echo "a soni juft";
+}
---
+++ boolean/boolean4-5.php
@@ -0,0 +1,21 @@
+<?php
+//Ikkita butun A va V sonlari berilgan. Jumlani rostlikka tekshiring: “A>2 va B<=3”
+/*
+$a = 4;
+$b = 2;
+if ($a>2 &&$b<=3){
+    echo "true";
+}
+else{
+    echo "false";
+}
+*/
+//Ikkita butun A va V sonlari berilgan. Jumlani rostlikka tekshiring: “A >= 0 yoki V < -2"
+$a = 2;
+$b = -5;
+if ($a>=0 && $b <-2){
+    echo "true";
+}
+else{
+    echo "false";
+}
