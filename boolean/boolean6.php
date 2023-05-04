2708e8c36b2d243f90d1dbca959732725330ef43
Matkarim Matkarimov <matkarim1099@gmail.com>
2023-05-04 23:33:40 +0500

    04.05.23

--- boolean/boolean6.php
+++ boolean/boolean6.php
@@ -1,1 +1,11 @@
 <?php
+//Uchta A, V, S butun sonlar berilgan. Jumlani rostlikka tekshiring: “A<= V <= S"
+$a = 2;
+$b = 3;
+$c = 3;
+if ($a<=$b && $b<=$c){
+    echo "true";
+}
+else{
+    echo "false";
+}
